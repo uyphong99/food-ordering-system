c826283f59b1fd1cb7d187e631d0bbe0f249b348
Phong Uy <uycuno1@outlook.com.vn>
2023-05-14 01:03:07 +0700

    Add avro schema in kafka-model

--- infrastructure/docker-compose/docker-compose-down.sh
+++ infrastructure/docker-compose/docker-compose-down.sh
@@ -3,4 +3,5 @@
 docker compose -f common.yml -f zookeeper.yml down
 docker compose -f common.yml -f kafka_cluster.yml down
 docker compose -f common.yml -f init_kafka.yml down
+docker compose -f common.yml -f postgres.yml down
 
--- infrastructure/docker-compose/docker-compose-up.sh
+++ infrastructure/docker-compose/docker-compose-up.sh
@@ -3,3 +3,4 @@
 docker compose -f common.yml -f zookeeper.yml up -d
 docker compose -f common.yml -f kafka_cluster.yml up -d
 docker compose -f common.yml -f init_kafka.yml up -d
+docker compose -f common.yml -f postgres.yml up -d
