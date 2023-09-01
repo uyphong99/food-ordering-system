939722b1f6c12fcc1f0e81215ff1b4324b0e15e9
Phong Uy <uycuno1@outlook.com.vn>
2023-09-01 16:41:17 +0700

    Adevelop customer service

--- customer-service/src/main/resources/init-data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO customer.customers(id, username, first_name, last_name)
-VALUES ('a34bd938-f939-11ed-be56-0242ac120002', 'user_1', 'First', 'User');
-
-INSERT INTO customer.customers(id, username, first_name, last_name)
-VALUES ('b2cfef0c-f939-11ed-be56-0242ac120002', 'user_2', 'Second', 'User');
